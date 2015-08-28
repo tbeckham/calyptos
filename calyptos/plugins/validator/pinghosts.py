cca9a39577fb11a0bcb6c9d16c91d3304c25ac8f
Casey Cassler <casey.cassler@eucalyptus.com>
2015-08-28 14:43:48 -0700

    Improve pinghost performance

--- calyptos/plugins/validator/pinghosts.py
+++ calyptos/plugins/validator/pinghosts.py
@@ -1,18 +1,39 @@
 from calyptos.plugins.validator.validatorplugin import ValidatorPlugin
-import os
+import threading
+import subprocess
+
 
 class PingHosts(ValidatorPlugin):
+    hostlock = threading.Lock()
+
     def validate(self):
+        results = {}
+        threads = []
+
+        def results_ping(host, count, hostlock):
+            res = self.ping_ip(host, count=count)
+            with hostlock:
+                results[host] = res
         for host in self.component_deployer.all_hosts:
-            if self._ping(host, count=3):
+            t = threading.Thread(target=results_ping, args=(host, 3, self.hostlock))
+            threads.append(t)
+            t.start()
+        for t in threads:
+            t.join()
+        for host, result in results.iteritems():
+            if result:
                 self.success('Ping to ' + host)
             else:
                 self.failure('Ping to ' + host)
                 raise AssertionError('Unable to ping host: ' + host)
 
-    def _ping(self, host, count=1):
-        exit_code = os.system('ping -c {0} {1}'.format(count, host))
-        if exit_code != 0:
+    def ping_ip(self, host, count=3):
+        args = 'ping -W 3 -c {0} {1}'.format(count, host)
+        process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+                                   bufsize=4096, shell=True)
+        output, unused_err = process.communicate()
+        retcode = process.poll()
+        if retcode:
+            print output
             return False
-        else:
-            return True
+        return True
