c5bf721a9c85c88cf621402049c9ef959f750651
tbeckham <tony.beckham@gmail.com>
2015-09-03 11:19:56 -0700

    Add "--no-update-repo" flag. If the flag is specified, prepare will *NOT* pull changes from the eucalyptus-cookbook origin.

--- calyptos/plugins/deployer/chef.py
+++ calyptos/plugins/deployer/chef.py
@@ -13,7 +13,7 @@ from calyptos.rolebuilder import RoleBuilder
 class Chef(DeployerPlugin):
     def __init__(self, password, environment_file='etc/environment.yml',
                  config_file='config.yml', debug=False, branch='euca-4.1',
-                 cookbook_repo='https://github.com/eucalyptus/eucalyptus-cookbook'):
+                 cookbook_repo='https://github.com/eucalyptus/eucalyptus-cookbook', update_repo=True):
         self.chef_repo_dir = 'chef-repo'
         self.environment_file = environment_file
         if debug:
@@ -23,13 +23,13 @@ class Chef(DeployerPlugin):
         self.role_builder = RoleBuilder(environment_file)
         self.roles = self.role_builder.get_roles()
         self.all_hosts = self.roles['all']
-        self._prepare_fs(cookbook_repo, branch, debug)
+        self._prepare_fs(cookbook_repo, branch, debug, update_repo)
         self.environment_name = self._write_json_environment()
         self.chef_manager = ChefManager(password, self.environment_name,
                                         self.roles['all'])
         self.config = self.get_chef_config(config_file)
 
-    def _prepare_fs(self, cookbook_repo, branch, debug):
+    def _prepare_fs(self, cookbook_repo, branch, debug, update_repo):
         ChefManager.install_chef_dk()
         ChefManager.create_chef_repo()
         with hide(*self.hidden_outputs):
@@ -37,8 +37,10 @@ class Chef(DeployerPlugin):
                   'git clone '
                   '{0} eucalyptus-cookbook;'
                   'fi'.format(cookbook_repo))
-            local('cd eucalyptus-cookbook; git checkout {0};'.format(branch))
-            local('cd eucalyptus-cookbook; git pull origin {0};'.format(branch))
+            if update_repo:
+                print green('updating eucalyptus-cookbook')
+                local('cd eucalyptus-cookbook; git checkout {0};'.format(branch))
+                local('cd eucalyptus-cookbook; git pull origin {0};'.format(branch))
         ChefManager.download_cookbooks('eucalyptus-cookbook/Berksfile',
                                        os.path.join(self.chef_repo_dir +
                                                     '/cookbooks'),
